59eeff091c7e7d275a4d5ebb5ce9b206e941b6ae
Michael Schwarz <michi.schwarz@gmail.com>
2013-02-01 18:32:19 +0100

    Added missing 'channel' tag.

--- gateway.py
+++ gateway.py
@@ -151,8 +151,9 @@ class Feed:
 		
 		return n(
 			'rss',
-			n('title', self.title),
-			*[i.make_podcast_entry_elem(gateway) for i in self.videos],
+			n('channel',
+				n('title', self.title),
+				*[i.make_podcast_entry_elem(gateway) for i in self.videos]),
 			xmlns__itunes = 'http://www.itunes.com/dtds/podcast-1.0.dtd',
 			version = '2.0')
 	
